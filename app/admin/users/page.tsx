dad62876c0abc73c4455458c83722c0138f215ad
Jayendra Bharti <jay.bharti2804@gmail.com>
2025-11-18 14:30:37 +0530

    added-admin-user-page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,10 @@
+import RevealHero from "@/components/animations/RevealHero";
+
+export default async function AdminUsersPage() {
+  return (
+    <section className="w-full text-center h-dvh p-4 flex flex-col gap-3">
+      <RevealHero className="font-extrabold text-3xl mx-auto">Users</RevealHero>
+      <span>Coming Soon</span>
+    </section>
+  );
+}
